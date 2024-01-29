62c9832387054e4ac141d8804c9ef669579ed56c
vaibhav <fullstackexpert301@gmail.com>
2024-01-29 11:33:11 +0530

    update insta live images

--- src/components/instaLive.jsx
+++ src/components/instaLive.jsx
@@ -5,6 +5,8 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import TextBootomIcon from "../images/Shape.png";
+import InstaImage3 from "../images/insta_3.jpg"
+import InstaImage4 from "../images/InstaImage_2.jpg"
 
 export default function InstaLive() {
   const [swiperRef, setSwiperRef] = useState(null);
@@ -15,13 +17,12 @@ export default function InstaLive() {
       setActiveIndex(swiperRef.activeIndex);
     }
   };
+
   const s1 =
     "https://scontent.cdninstagram.com/v/t39.30808-6/412034277_829163182553313_7801524505859840587_n.jpg?stp=dst-jpg_e15&efg=e30&_nc_ht=scontent.cdninstagram.com&_nc_cat=109&_nc_ohc=iQ486wmR7skAX_9lKUD&edm=APs17CUAAAAA&ccb=7-5&oh=00_AfDjEOg4QCb6NdhdP0aVdowJuplUgpUHOuX3R2xlWJAkQw&oe=65B8441F&_nc_sid=10d13b";
-  const s2 =
-    "https://scontent.cdninstagram.com/v/t39.30808-6/415088773_842178384585126_7151393333812846566_n.jpg?stp=dst-jpg_e15_s750x750&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDAweDEwMDAuc2RyIn0&_nc_ht=scontent.cdninstagram.com&_nc_cat=106&_nc_ohc=A-DyAenBIXAAX-j-r_5&edm=APs17CUAAAAA&ccb=7-5&oh=00_AfDQU8t7baCxy1cCcUVTyHD9YI-IAo9It-xlDMVYpV9A6A&oe=65B6DA81&_nc_sid=10d13b";
   const s3 =
     "https://scontent.cdninstagram.com/v/t39.30808-6/415054086_841351844667780_7938624697302372151_n.jpg?stp=dst-jpg_e15&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDAweDEwMDAuc2RyIn0&_nc_ht=scontent.cdninstagram.com&_nc_cat=100&_nc_ohc=xxWz4tOuodkAX8zsH9Z&edm=APs17CUAAAAA&ccb=7-5&oh=00_AfBe5e4PS7v0q4XO2lLaoigq-M8tw8xD5dBt34HC9xfMwg&oe=65B7A4F8&_nc_sid=10d13b";
-  const images = [s1, s3, s2, s1, s3, s2];
+  const images = [s1,s3 ,InstaImage3,InstaImage4, s1, s3, InstaImage3,InstaImage4];
 
   const handlePrevClick = () => {
     if (swiperRef) {
