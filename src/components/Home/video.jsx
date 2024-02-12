21f94c1809d0952c8ccd8c726c67859d8875ab0b
vaibhav_rathore <vaibahvrathorema@gmail.com>
2024-02-12 13:21:49 +0530

    update video on home page

--- src/components/Home/video.jsx
+++ src/components/Home/video.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import "../../style/video.css";
 function YTVideo() {
-    const videoUrl = "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+  // const videoUrl = Saburi_video;
   return (
     <>
       <div className="video-container">
-      <video controls width="100%" height="auto">
-            <source src={videoUrl} type="video/mp4" />
+        {/* <video controls width="100%" height="auto" autoPlay={true}>
+            <source src={Saburi_video} type="video/mp4"  />
             Your browser does not support the video tag.
-          </video>
+          </video> */}
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/yY3bTiygD5Q?si=uPyfXM4ioBovW8bm"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+          className="yout_video_iframe"
+        ></iframe>
       </div>
     </>
   );
--- src/components/contactuscomponent/haveaquestion.jsx
+++ src/components/contactuscomponent/haveaquestion.jsx
@@ -1,45 +1,43 @@
-import React from 'react'
+import React from "react";
 import "../../style/contact-us/have-a-question-contact-us-header.css";
-import Mylocation from './mylocation';
-
+import Mylocation from "./mylocation";
 
 function Haveaquestion() {
   return (
     <>
-    <div className='main-have-a-question-contact-us' >
-          <Mylocation/>
-      {/* have a question */}
-      <div>
-        {/* form */}
-        <div className='have_a_question_main_form_before_start' >
-          <div className='text-start' >
-        <h1>Have a question?</h1>
+      <div className="main-have-a-question-contact-us">
+        <Mylocation />
+        {/* have a question */}
+        <div>
+          {/* form */}
+          <div className="have_a_question_main_form_before_start">
+            <div className="text-start ">
+              <h1 className="have_a_question_text">Have a question?</h1>
+            </div>
+            {/* form */}
+            <div className="have_a_question_main_form">
+              <div className="have_a_question_main_form_input_div">
+                <input type="text" name="Full_Name" placeholder="Full Name" />
+              </div>
+              <div className="have_a_question_main_form_input_div">
+                <input type="text" name="email" placeholder="Email Address" />
+              </div>
+              <div className="have_a_question_main_form_input_div">
+                <input type="text" name="phone" placeholder="Phone Number" />
+              </div>
+              <div className="have_a_question_main_form_input_div">
+                <textarea type="text" name="message" placeholder="Message" />
+              </div>
+              {/* button */}
+              <div className="have_a_question_save_button_div ">
+                <button className="have_a_question_save_button ">Submit</button>
+              </div>
+            </div>
           </div>
-        {/* form */}
-        <div className='have_a_question_main_form' >
-          <div className='have_a_question_main_form_input_div' >
-            <input type="text" name="Full_Name" placeholder='Full Name' />
-          </div>
-          <div className='have_a_question_main_form_input_div' >
-            <input type="text" name="email" placeholder='Email Address' />
-          </div>
-          <div className='have_a_question_main_form_input_div' >
-            <input type="text" name="phone" placeholder='Phone Number' />
-          </div>
-          <div className='have_a_question_main_form_input_div' >
-            <textarea type="text" name="message" placeholder='Message' />
-          </div>
-          {/* button */}
-          <div className='have_a_question_save_button_div ' >
-            <button className='have_a_question_save_button ' >Submit</button>
-          </div>
-        </div>
         </div>
       </div>
-    </div>
-
     </>
-  )
+  );
 }
 
-export default Haveaquestion
+export default Haveaquestion;
