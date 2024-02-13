fd960c582fcccff1a0100ee96d1d5b6306d7c7c0
vaibhav_rathore <vaibahvrathorema@gmail.com>
2024-02-13 16:33:55 +0530

    updat testimonial images

--- src/components/Home/testimonials.jsx
+++ src/components/Home/testimonials.jsx
@@ -24,7 +24,6 @@ export default function Testimonial() {
     {
       src: image1,
       user : "https://badmchampionship.nmims.edu/wp-content/uploads/2017/10/manish-desai.jpg",
-      // user:"https://health-equity-action.org/sites/default/files/styles/large/public/pictures/2023-01/Naresh-Punjabi-2000x2000.jpg?itok=oqH0VzPn",
 
       message:
         "Exquisite teas, exceptional flavors. Saburi elevates my tea experience. Quality, taste, and packaging, all perfect!",
@@ -32,7 +31,6 @@ export default function Testimonial() {
     },
     {
       src: image1,
-      // user:"https://health-equity-action.org/sites/default/files/styles/large/public/pictures/2023-01/Naresh-Punjabi-2000x2000.jpg?itok=oqH0VzPn",
       user:"https://media.licdn.com/dms/image/D4D03AQHUCyN6xKF26w/profile-displayphoto-shrink_800_800/0/1684568848914?e=2147483647&v=beta&t=OreURQwOJ3k2GYgb_9AHro6ofuzNgrRppEMnQyShelc",
       message:
         "Spice up your life with Saburi. Exceptional flavors and quality. My dishes have never tasted this better!",
@@ -132,7 +130,7 @@ export default function Testimonial() {
                   />
                   <div className="testiminal_user_image_div">
                     <img
-                      src={UserImage}
+                       src={testimonial.user}
                       alt="user_image"
                       className="testiminal_user_image"
                     />
